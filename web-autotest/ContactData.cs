4f46610b34d4694ce6a850a3204963fcaad07893
xxlxep <xxlxep@gmail.com>
2021-05-22 20:28:17 +0300

    3.  Adding New contact test. Adding ContactData class

---
+++ web-autotest/ContactData.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace web_autotest
+{
+    class ContactData
+    {
+        private string firstname;
+        private string lastname;
+
+        public ContactData(string username, string password)
+        {
+            this.firstname = username;
+            this.lastname = password;
+
+        }
+
+        public string FirstName
+        {
+            get
+            {
+                return firstname;
+            }
+            set
+            {
+                firstname = value;
+            }
+        }
+        public string LastName
+        {
+            get
+            {
+                return lastname;
+            }
+            set
+            {
+                lastname = value;
+            }
+        }
+
+
+    }
+}
